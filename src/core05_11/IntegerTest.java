d94edf83211a3998571a956d9c57a5588a6a3735
jj <2380228977@qq.com>
2020-05-21 23:22:19 +0800

    反射的例子代码

---
+++ src/core05_11/IntegerTest.java
@@ -0,0 +1,9 @@
+package core05_11;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer a = 100;
+        Integer b = 100;
+        System.out.println(a == b);
+    }
+}
--- src/core05_13/ReflectionTest.java
+++ src/core05_13/ReflectionTest.java
@@ -1,6 +1,6 @@
 package core05_13;
 
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.util.Scanner;
 
 public class ReflectionTest {
@@ -27,9 +27,75 @@ public class ReflectionTest {
                 System.out.print(" extends" + supercl.getName());
             }
             System.out.print("\n{\n");
-            
+            printConstructors(cl);
+            System.out.println();
+            printMethods(cl);
+            System.out.println();
+            printFields(cl);
+            System.out.println();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void printConstructors(Class cl){
+        Constructor[] constructors = cl.getConstructors();
+
+        for(Constructor constructor : constructors){
+            String name = constructor.getName();
+            System.out.print("    ");
+            String modifiers = Modifier.toString(constructor.getModifiers());
+            if(modifiers.length() > 0){
+                System.out.print(modifiers + " ");
+            }
+            System.out.print(name + "(");
+            Class[] parameter = constructor.getParameterTypes();
+            for(int j = 0; j < parameter.length; j++){
+                if (j > 0){
+                    System.out.print(", ");
+                }
+                System.out.print(parameter[j].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
+    private static void printMethods(Class cl){
+        Method[] methods = cl.getMethods();
+
+        for(Method method : methods){
+            String name = method.getName();
+            Class retType = method.getReturnType();
+
+            System.out.printf("     ");
+            String modifiers = Modifier.toString(method.getModifiers());
+            if(modifiers.length() > 0){
+                System.out.printf(modifiers + " ");
+            }
+            System.out.printf(retType.getName() + " " + name + " (");
+            Class[] paramTypes = method.getParameterTypes();
+            for (int j = 0;j < paramTypes.length; j++){
+                if(j > 0){
+                    System.out.printf(", ");
+                }
+                System.out.printf(paramTypes[j].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
+    private static void printFields(Class cl){
+        Field[] fields = cl.getFields();
+
+        for (Field field : fields){
+            Class type = field.getType();
+            String name = field.getName();
+            System.out.printf("     ");
+            String modifies = Modifier.toString(field.getModifiers());
+            if(modifies.length() > 0){
+                System.out.print(modifies + "");
+            }
+            System.out.println(type.getName() + " " + name + ";");
         }
     }
 }
